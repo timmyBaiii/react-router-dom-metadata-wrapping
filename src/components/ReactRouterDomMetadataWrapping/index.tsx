2683f9abaeada20d199a08638ddb009bae0ffecb
root <>
2023-11-29 08:48:52 +0800

    Intial commit

--- src/components/ReactRouterDomMetadataWrapping/index.tsx
+++ src/components/ReactRouterDomMetadataWrapping/index.tsx
@@ -1,87 +1,11 @@
-import React, { useEffect } from 'react';
-import { useLocation, useRoutes } from 'react-router-dom';
-
-// utils
-import { isExternal } from '@/utils/validate';
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
 
 // types
-import { ReactRouterDomMetadataWrappingPropsType, ReactRouterDomMetadataWrappingType } from './types';
+import { ReactRouterDomMetadataWrappingPropsType } from './types';
 
-const resolvePath = (routePath: any, basePath: string) => {
-    if (isExternal(routePath)) {
-        return routePath;
-    }
-
-    if (isExternal(basePath)) {
-        return basePath;
-    }
-
-    if (routePath.indexOf('/') !== -1) {
-        return basePath + routePath;
-    }
-    else {
-        return basePath + '/' + routePath;
-    }
-};
-
-const ReactRouterDomMetadataWrapping: React.FC<ReactRouterDomMetadataWrappingPropsType> = ({
-    routes,
-    onRouteBefore
-  }: ReactRouterDomMetadataWrappingPropsType) => {
+function ReactRouterDomMetadataWrapping({ routes, onRouteBefore }: ReactRouterDomMetadataWrappingPropsType) {
     const element = useRoutes(routes);
-    const location = useLocation();
-
-    // const extractRoutes = (routes: any, parentPath = '') => {
-    //     const result = [];
-
-    //     for (const route of routes) {
-    //         const { path, children, meta } = route;
-
-    //         const fullPath = resolvePath(path, parentPath);
-
-    //         if (children) {
-    //             const childRoutes: any = extractRoutes(children, fullPath);
-    //             result.push(...childRoutes);
-    //         }
-    //         else {
-    //             result.push({
-    //                 fullPath,
-    //                 meta
-    //             });
-    //         }
-    //     }
-
-    //     return result;
-    // };
-
-    // useEffect(() => {
-    //     if (element?.props?.match?.route?.children && element?.props?.match?.pathnameBase && onRouteBefore) {
-    //         const routeList = extractRoutes(element?.props?.match?.route.children, element?.props?.match.pathnameBase);
-
-    //         let activeRoute = 0;
-    //         for (let i = 0; i < routeList.length; i++) {
-    //             if (routeList[i].fullPath === location.pathname) {
-    //                 activeRoute = i;
-    //                 break;
-    //             }
-    //         }
-
-    //         onRouteBefore(routeList[activeRoute]);
-    //     }
-    //     else {
-    //         const pathnameBase = element?.props?.match?.pathnameBase;
-    //         const meta = element?.props?.match?.route?.meta;
-
-    //         if (pathnameBase && meta && onRouteBefore) {
-    //             const result = {
-    //                 pathname: pathnameBase,
-    //                 meta: meta
-    //             };
-
-    //             onRouteBefore(result);
-    //         }
-    //     }
-    // }, [location.pathname]);
 
     return element;
 };
